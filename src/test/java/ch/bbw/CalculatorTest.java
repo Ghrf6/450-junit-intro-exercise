2ec270d67a29898b58fcdc2d228111c0218c7fa1
Ghrf6 <bbw.swica@gmail.com>
2025-10-02 15:45:25 +0200

    Subtraction added

--- src/test/java/ch/bbw/CalculatorTest.java
+++ src/test/java/ch/bbw/CalculatorTest.java
@@ -1,13 +1,20 @@
 package ch.bbw;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled("für die Übung")
 class CalculatorTest {
-    Calculator calculator;
+
+Calculator calculator;
+
+@BeforeEach
+void setUp() {
+    calculator = new Calculator();
+}
 
     @Test
     void add_1_plus_1() {
@@ -18,4 +25,56 @@ class CalculatorTest {
 
         assertEquals(a + b, result, "addition failed");
     }
+
+    @Test
+    void add_2_plus_2() {
+        int a = 2;
+        int b = 2;
+
+        int result = calculator.add(a, b);
+
+        assertEquals(a + b, result, "addition failed");
+    }
+
+    @Test
+    void add_1_plus_2() {
+        int a = 1;
+        int b = 2;
+
+        int result = calculator.add(a, b);
+
+        assertTrue(result == a + b, "addition failed");
+    }
+
+    @Test
+    void subtract_1_minus_1() {
+        int a = 1;
+        int b = 1;
+
+        int result = calculator.subtract(a, b);
+
+        assertEquals(a - b, result, "subtraction failed");
+    }
+
+    @Test
+    void subtract_2_minus_1() {
+        int a = 2;
+        int b = 1;
+
+        int result = calculator.subtract(a, b);
+
+        assertEquals(a - b, result, "subtraction failed");
+    }
+
+    @Test
+    void subtract_3_minus_5() {
+        int a = 3;
+        int b = 5;
+
+        int result = calculator.subtract(a, b);
+
+        assertTrue(result == a - b, "subtraction failed");
+    }
+
+
 }
